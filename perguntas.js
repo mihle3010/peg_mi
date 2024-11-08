59893f08540eed7a582f4a0ad98087ef24c8816b
mihle3010 <162493948+mihle3010@users.noreply.github.com>
2024-11-08 11:42:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ciências',
+    'Por que as plantas precisam de luz para crescer?',
+    'As plantas precisam de luz para crescer porque usam a luz solar para fazer fotossíntese. Esse processo permite que elas produzam o próprio alimento a partir da água e gás carbônico.'
+)
+
+criaCartao(
+    'Matemática',
+    'Se você tem 10 balas e dá 3 para um amigo, quantas balas você fica?',
+    'Se você tem 10 balas e dá 3 para um amigo, fica com 7 balas'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o nome do maior rio do Brasil',
+    'O maior rio do Brasil é o Rio Amazonas'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Marechal Deodoro da Fonseca'
+)
